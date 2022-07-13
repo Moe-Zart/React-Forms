7e071d2dcda9e3eb6a1546e7fde812507daf1129
Moe-Zart <mohamedzreika03@outlook.com>
2022-07-14 01:25:05 +1000

    checkbox

--- src/components/Forms1.jsx
+++ src/components/Forms1.jsx
@@ -5,7 +5,8 @@ class Forms1 extends React.Component {
         super()
         this.state ={
             firstName:"",
-            lastName:""
+            lastName:"",
+            isFriendly:true
         }
         this.handleChange = this.handleChange.bind(this)
     }
@@ -14,8 +15,9 @@ class Forms1 extends React.Component {
         //we want the firstName state to change to the current value of the input box.
         /* when an event fires (e.g: the letters passing into the input area), 
             we pass the predetermined event parameter into the method */
-        this.setState({[event.target.name]: event.target.value}) //the target is the input area
-        //this works because the target name, aka the name of the input below is the same as the states above
+        event.target.type === "checkbox" ? this.setState({[event.target.name]:event.target.checked}):this.setState({[event.target.name]: event.target.value}) //the target is the input area
+        //this works because the target name, aka the name of the inputs below are the same as the states above
+        console.log(event.target.checked)
     }
   render() {
     return (
@@ -24,6 +26,7 @@ class Forms1 extends React.Component {
         <br />
         <input type="text" name="lastName" placeholder="Last Name" onChange={this.handleChange}/>
         <h1>{this.state.firstName} <br />{this.state.lastName}</h1>
+        <input type="checkbox" name="isFriendly" checked={this.state.isFriendly} onChange={this.handleChange}/>
       </form>
     );
   }
